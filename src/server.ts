a217fe67bd7f1c79756fb1db2c89b785ae8041d9
Corey Alexander <coreyja@gmail.com>
2020-05-15 22:13:58 -0400

    Get basic websocket server working. it sends the time to the client

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
 import express from 'express';
+import { Server } from 'ws';
 
 const PORT = process.env.PORT || 3000;
 
-const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express()
+  .use((_req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server: server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);

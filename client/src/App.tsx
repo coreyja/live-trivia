e37ad2a7567b22e8bb1a1780deb844a11e01f858
Corey Alexander <coreyja@gmail.com>
2020-05-16 14:27:58 -0400

    Client can now connect to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import useWebSocket from 'react-use-websocket';
+import React from "react";
+import useWebSocket from "react-use-websocket";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const socketUrl = 'wss://echo.websocket.org';
+  const socketUrl = "ws://localhost:3000";
 
-const {
-  sendMessage,
-  sendJsonMessage,
-  lastMessage,
-  lastJsonMessage,
-  readyState,
-  getWebSocket
-} = useWebSocket(socketUrl, {
-  onOpen: () => console.log('opened'),
-  //Will attempt to reconnect on all close events, such as server shutting down
-  shouldReconnect: (closeEvent) => true,
-});
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("opened"),
+    //Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (_closeEvent) => true,
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Will this auto-reload...
-          Edit <code>src/App.tsx</code> and save to reload.
+          Will this auto-reload... Edit <code>src/App.tsx</code> and save to
+          reload.
         </p>
+        <p>{lastMessage && lastMessage.data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"

f19c020d42cc746ef132ac514a0f59da4888951d
Corey Alexander <coreyja@gmail.com>
2020-05-16 14:03:31 -0400

    Install websocket package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import useWebSocket from 'react-use-websocket';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const socketUrl = 'wss://echo.websocket.org';
+
+const {
+  sendMessage,
+  sendJsonMessage,
+  lastMessage,
+  lastJsonMessage,
+  readyState,
+  getWebSocket
+} = useWebSocket(socketUrl, {
+  onOpen: () => console.log('opened'),
+  //Will attempt to reconnect on all close events, such as server shutting down
+  shouldReconnect: (closeEvent) => true,
+});
+
   return (
     <div className="App">
       <header className="App-header">

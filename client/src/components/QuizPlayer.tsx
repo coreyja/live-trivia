e6983e0674a1a13ef08aa5f15766cc67190b0de5
Corey Alexander <coreyja@gmail.com>
2020-05-20 21:45:56 -0400

    Ability to answer quesion on the frontend

--- client/src/components/QuizPlayer.tsx
+++ client/src/components/QuizPlayer.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import useWebSocket from "react-use-websocket";
 
 import { WEBSOCKET_HOST } from "../urls";
+import { WebsocketMessage } from "server/src/models";
 
 interface Props {
   userName: string;
 }
 
-const QuizPlayer = (props: Props) => {
+const QuizPlayer: FunctionComponent<Props> = (props) => {
   const { lastJsonMessage, sendMessage } = useWebSocket(
     `${WEBSOCKET_HOST}/ws`,
     {
@@ -15,20 +16,45 @@ const QuizPlayer = (props: Props) => {
     }
   );
 
-  return (
-    <div>
-      <h1>Hello {props.userName}</h1>
-      <p>{lastJsonMessage && JSON.stringify(lastJsonMessage)}</p>
+  const message = lastJsonMessage as WebsocketMessage | undefined;
 
-      <button
-        onClick={() => {
-          sendMessage("test");
-        }}
-      >
-        Submit
-      </button>
-    </div>
-  );
+  if (!message) {
+    return <>No Messages Yet</>;
+  } else if (message.event === "connected") {
+    return <>The time is: {message.time}</>;
+  } else if (message.event === "question") {
+    const q = message.question;
+
+    const answerQuestion = (answer: string): void => {
+      sendMessage(
+        JSON.stringify({
+          event: "answer-question",
+          question_id: q.id,
+          answer: answer,
+        })
+      );
+    };
+
+    return (
+      <div>
+        <h1>Hello {props.userName}</h1>
+        <h3>Time Left: {q.secondsLeft}</h3>
+        <p>{q.text}</p>
+
+        {q.answers.map((a) => (
+          <button
+            onClick={() => {
+              answerQuestion(a.text);
+            }}
+          >
+            {a.text}
+          </button>
+        ))}
+      </div>
+    );
+  } else {
+    return <>Error state</>;
+  }
 };
 
 export default QuizPlayer;
